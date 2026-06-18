c1970d990bafed1078ef7dcb154267cdc9f67e2e
harshvardhan3830 <vardhan3830@gmail.com>
2026-06-18 21:00:04 +0530

    Add basic arithmetic operation and enhance hello world examples

---
+++ class1.py
@@ -0,0 +1,32 @@
+# a='hello'
+# print(type(a))
+# print(a)
+# a=float(5)
+# print(type(a))
+# print(a)
+
+# b = False
+# print(type(b))
+#  int, str, float, boolean
+
+"""
+ hello this is harshvardhan
+ i am your python instructor
+ i am your python instructor
+ i am your python instructor
+ i am your python instructor
+"""
+
+# name = input("Enter your name : ")
+# age = input("Enter your age")
+
+# print(type(name))
+# print(type(age))
+
+# WAP that adds two numbers and take the numbers as input
+
+a = int(input("Enter first number : "))
+b = int(input("Enter secound number : "))
+
+
+print(a+b)

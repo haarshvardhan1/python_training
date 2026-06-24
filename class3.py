3d3d5e054c3eb2660d786b4d7a639f16d4e45859
harshvardhan3830 <vardhan3830@gmail.com>
2026-06-24 21:24:23 +0530

    Add initial implementation of loops in class3.py and create README.md

---
+++ class3.py
@@ -0,0 +1,50 @@
+# Loops
+#  While & for
+# print(3)
+# print(2)
+# print(1)
+# print("GO")
+
+# While loop
+count = 10
+#  while condition: -> terminationg condition / if not terminating condition while will run till stackoverflow
+while count > 0:
+    print(f"Launching in T-minus {count} secound")
+    # count -= 1
+
+# print(f"🚀 Take Off")
+
+
+# For Loop
+#  for(int count=0; count<10; count++){}
+#
+# for count in range(1, 11, 2):
+#     print(count)
+
+
+# name = "HARSHVARDHAN"
+# for character in name:
+#     print(character.lower())
+
+# Loop controls / break and continue
+
+# for room in range(1, 11):
+#     print(f"Checking in room {room}")
+#     if room == 4:
+#         print(f"Found the key in room {room}")
+#         break
+
+
+# Continue
+
+# for floor in range(1, 11):
+#     if floor <= 5:
+#         continue #skip for the valid condition
+
+#     print(f"Stopping on floor {floor}")
+
+
+# for floor in range(1,11):
+#     pass
+
+

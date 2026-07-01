f112b5032bc556f33e5f0c9dc524476c052ce615
harshvardhan3830 <vardhan3830@gmail.com>
2026-07-01 19:59:28 +0530

    Enhance task manager functionality with task name, mark as done, and delete options

--- mini_cli.py
+++ mini_cli.py
@@ -23,12 +23,22 @@ def save_tasks(tasks):
         json.dump(tasks, file, indent=4)
 
 
+# tasks = []
+
+
 def add_task(tasks):
+
+    name = input("Enter task name : ").strip()
     description = input("Enter task description : ").strip()
 
-    task = {"id": len(tasks) + 1, "description": description, "done": False}
+    task = {
+        "id": len(tasks) + 1,
+        "name": name,
+        "description": description,
+        "done": False,
+    }
     tasks.append(task)
-    print(f"Task added: {description} \n")
+    print(f"✅ Task added with name {name} \n")
 
 
 def view_task(tasks):
@@ -36,10 +46,43 @@ def view_task(tasks):
         print("No Task yet. Add one!\n")
         return
 
-    print("\n --- Your Tasks ---")
+    print("\n -------------------- Your Tasks ---------------------")
     for task in tasks:
         status = "✅ Done" if task["done"] else "❌ Pending"
-        print(f"[{task['id']}] {task['description']} - {status}")
+        print(
+            f"[{task['id']}] Task Name : {task['name']} \n \t Task Description : {task['description']} \n \t Status: {status} \n"
+        )
+    print("*********************************************** \n\n")
+
+
+def mark_task_done(tasks):
+    if not tasks:
+        return
+
+    task_id = int(input("Enter task ID to mark as done : ").strip())
+
+    for task in tasks:
+        if task["id"] == task_id:
+            task["done"] = True
+            print(f"🎉 Task {task_id} marked as done!")
+            return
+
+    print(f"⚠️ No task found with ID {task_id}. \n")
+
+
+def delete_task(tasks):
+    if not tasks:
+        return
+
+    task_id = int(input("Enter task ID to delete task : ").strip())
+
+    for task in tasks:
+        if task["id"] == task_id:
+            tasks.remove(task)
+            print(f"🗑️ Task {task_id} deleted. \n")
+            return
+
+    print(f"⚠️ No task found with ID {task_id}. \n")
 
 
 def show_menu():
@@ -54,18 +97,27 @@ def show_menu():
 
 def main():
     tasks = load_tasks()
-    print("Welcome to your Task Manager! \n")
+    print("🗒️ Welcome to your Task Manager! \n")
 
-    show_menu()
-    choice = input("Choose an option (1-5) : ").strip()
+    while True:
+        show_menu()
+        choice = input("Choose an option (1-5) : ").strip()
 
-    if choice == "1":
-        add_task(tasks)
-    elif choice == "2":
-        view_task(tasks)
+        if choice == "1":
+            add_task(tasks)
+        elif choice == "2":
+            view_task(tasks)
+        elif choice == "3":
+            mark_task_done(tasks)
+        elif choice == "4":
+            delete_task(tasks)
+        elif choice == "5":
+            print("👋 Goodbye")
+            break
+        else:
+            print("⚠️Invalid choice. Please pick 1-5. \n")
 
-    save_tasks(tasks)
-    print("Goodbye")
+        save_tasks(tasks)
 
 
 if __name__ == "__main__":
